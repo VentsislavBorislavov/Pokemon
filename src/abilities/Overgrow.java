83c82558abf509c145a25fcab252303473992e05
Vencislav Borislavov <64546764+VentsislavBorislavov@users.noreply.github.com>
2020-05-24 13:19:35 +0300

    Added Bulbasaur pokemon

---
+++ src/abilities/Overgrow.java
@@ -0,0 +1,15 @@
+package abilities;
+
+import pokemon.Type;
+
+public class Overgrow implements Ability{
+
+    @Override
+    public int dealDamage(int damage, Type attackedPokemonType) {
+        if (attackedPokemonType == Type.EARTH)
+            return (int) (damage * 0.1);
+        if (attackedPokemonType == Type.WATER)
+            return (int) (damage * 0.8);
+        else return (int) (damage * 0.5);
+    }
+}
---
+++ src/pokemon/Bulbasaur.java
@@ -0,0 +1,18 @@
+package pokemon;
+
+import abilities.Overgrow;
+
+public class Bulbasaur extends Pokemon {
+    private static final String DEFAULT_NAME = "Bulbasaur";
+    private static final Type TYPE = Type.PLANT;
+
+    public Bulbasaur(int health, int defence, int attack, double height, double weight, Size size) {
+        super(DEFAULT_NAME, health, defence, attack, height, weight, TYPE, size);
+        abilities.add(new Overgrow());
+    }
+
+    @Override
+    public int useAbility1(Type attackedPokemonType) {
+        abilities.get(1).dealDamage(attack, attackedPokemonType);
+    }
+}
